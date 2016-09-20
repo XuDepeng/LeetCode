0c902213119e3a9571aac9b4224be0388d5d8308
许德朋 <depp593744989@gmail.com>
2016-09-20 21:00:54 +0800

    Merge Two Sorted Lists  finished

--- MergeTwoSortedLists/main.cpp
+++ MergeTwoSortedLists/main.cpp
@@ -1,4 +1,9 @@
 #include <cstdlib>
+#include <cstdio>
+
+#include <iostream>
+
+#include <vector>
 
 using namespace std;
 
@@ -15,8 +20,37 @@ public:
 	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
 		ListNode* h = new ListNode(0);
 
+		ListNode* node = h;
 		ListNode* n1 = l1;
 		ListNode* n2 = l2;
+
+		while (n1 && n2) {
+			if (n1->val < n2->val) {
+				ListNode* tmp = n1;
+				n1 = n1->next;
+				tmp->next = nullptr;
+				node->next = tmp;
+			}
+
+			else {
+				ListNode* tmp = n2;
+				n2 = n2->next;
+				tmp->next = nullptr;
+				node->next = tmp;
+			}
+
+			node = node->next;
+		}
+
+		if (n1) {
+			node->next = n1;
+		}
+
+		if (n2) {
+			node->next = n2;
+		}
+
+		return h->next;
 	}
 };
 
